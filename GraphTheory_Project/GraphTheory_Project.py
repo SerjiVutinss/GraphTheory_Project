042d0d262535b029faa695e5013fe2d1393289f7
SerjiVutinss <SerjiVutinss@gmail.com>
2019-03-17 02:04:15 +0000

    Initial commit

--- GraphTheory_Project/GraphTheory_Project.py
+++ GraphTheory_Project/GraphTheory_Project.py
@@ -1,1 +1,66 @@
+import thompsons_constructor
+import shunting_yard
 
+print(thompsons_constructor.compile("ab.cd.|"))
+print(thompsons_constructor.compile("aa.*"))
+
+print(shunting_yard.shunt("(a.b)|(c*.d)"))
+print(shunting_yard.shunt("(a|b).(c*.d)"))
+
+infixes = ["a.b.c", "a.(b|d).c*", "(a.(b|d))*", "a.(b.b)*.c"]
+strings = ["", "abc", "abbc", "abcc", "abad", "abbbc"]
+
+def followEedges(state):
+	"""Returns the set of states that can be reached from
+		the state, following the arrows"""
+
+	# create a new set with state as its only member
+	states = set()
+	states.add(state)
+	# if state has e arrows
+	if(state.label is None):
+		# check if edge one is a state
+		if(state.edge1 is not None):
+			# recursively follow the edges
+			# union states
+			states |= followEedges(state.edge1)
+		# check if edge one is a state
+		if(state.edge2 is not None):
+			# recursively follow the edges
+			# union states
+			states |= followEedges(state.edge2)
+
+	# return the set of states
+	return states
+
+def match(infix, string):
+	"""Match string to infix reg ex"""
+
+	# shunt and compile the reg ex
+	postfix = shunting_yard.shunt(infix)
+	nfa = thompsons_constructor.compile(postfix)
+
+	# set of current and next states
+	currentStates = set()
+	nextStates = set()
+
+	# add initial state of nfa to current states
+	currentStates |= followEedges(nfa.initial)
+
+	# loop through each character in the string
+	for s in string:
+		# loop through current states
+		for c in currentStates:
+			# check if state is labelled s
+			if(c.label == s):
+				# follow its E edge (will only be one)
+				nextStates |= followEedges(c.edge1)
+		# set current to next and clear next set
+		currentStates = nextStates
+		nextStates = set()
+	# return state is in set of current states
+	return (nfa.accept in currentStates)
+
+for i in infixes:
+	for s in strings:
+		print match(i, s), i, s
---
+++ GraphTheory_Project/thompsons_constructor.py
@@ -0,0 +1,73 @@
+# Thompson's Algorithm
+
+# use None for a label representing 'E' arrows
+class state:
+	label = None
+	edge1 = None
+	edge2 = None
+
+
+class nfa:
+	initial = None
+	accept = None
+
+	def __init__(self, initial, accept):
+		self.initial = initial
+		self.accept = accept
+
+def compile(postfix):
+	nfaStack = []
+
+	for c in postfix:
+		if(c == '.'):
+			nfa2 = nfaStack.pop()
+			nfa1 = nfaStack.pop()
+
+			# join the two nfas together
+			nfa1.accept.edge1 = nfa2.initial
+			nfaStack.append(nfa(nfa1.initial, nfa2.accept))
+
+		elif(c == '|'):
+			# pop two nfas from the stack
+			nfa2 = nfaStack.pop()
+			nfa1 = nfaStack.pop()
+			# create new initial and accept states
+			initial = state()
+			accept = state()
+			# connect the initial state to the initial states of the nfas
+			initial.edge1 = nfa1.initial
+			initial.edge2 = nfa2.initial
+			# connect the accept state to the accept states of the nfas
+			nfa1.accept.edge1 = accept
+			nfa2.accept.edge1 = accept
+			# push a new nfa to the stack, using accept and initial states
+			nfaStack.append(nfa(initial, accept))
+
+		elif (c == '*'):
+			# pop single nfa
+			nfa1 = nfaStack.pop()
+			# create new initial and accept states
+			initial = state()
+			accept = state()
+			# join the new initial state to the nfa initial state
+			# and the new accept state
+			initial.edge1 = nfa1.initial
+			initial.edge2 = accept
+			# join old acept state to new accept state and nfa1's
+			# initial state
+			nfa1.accept.edge1 = nfa1.initial
+			nfa1.accept.edge2 = accept
+			# push new nfa to stack
+			nfaStack.append(nfa(initial, accept))
+		else:
+			initial = state()
+			accept = state()
+
+			initial.label = c # label is char
+			initial.edge1 = accept # point to the accept state
+
+			# create a new nfa using the two states and push to stack
+			nfaStack.append(nfa(initial, accept))
+
+	# only should be one nfa on stack at this point
+	return nfaStack.pop()
