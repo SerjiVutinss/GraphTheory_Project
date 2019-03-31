ebea4d176ec7692a24714191370d6710fe1c8b1f
SerjiVutinss <SerjiVutinss@gmail.com>
2019-03-31 22:25:44 +0100

    Updated menu with toggles and options for plotting and saving to file

--- GraphTheory_Project/app.py
+++ GraphTheory_Project/app.py
@@ -5,57 +5,93 @@ import utils
 
 def main():
     print "Starting..."
-    #runMatchTest()
     menuLoop()
 
 def menuLoop():
     keepRunning = True
+    isPlotting = False
+    isWriteToFile = False
+
+    infix_file_name = None
+    string_file_name = None
+    output_file_name = None
+
     while(keepRunning):
+        print ""
+        print "------------------------"
+        print "Plotting: " + str(isPlotting)
+        print "Write to file: " + str(isWriteToFile)
+        print "------------------------"
+
         print "Please make a selection:"
         print "\t1) Manually input a reg-ex and string to be matched"
-        print "\t2) Run test suite of strings and infixes"
-        print "\t9) Load strings and infixes from sample files"
-        print  "\tq)uit"
+        print "\t2) Specify a file containing infixes and a file containing strings"
+        print "\t3) Run test suite of strings and infixes"
+        print "Options:"
+        print "\tp) Toggle plot graph for each result"
+        print "\tw) Toggle write to CSV file for result set"
+        print  "\tq)uit\n"
         selection = raw_input('Please choose: ')
 
+        print ""
         if(selection == '1'):
-            print "You selected 1"
+            print "You selected 1 - manually input regex and string"
+            infix = raw_input("Please enter the infix regex: ")
+            string = raw_input("Please enter the string to match: ")
+            print "\nResult"
+            if len(infix) == 0:
+                print "Infix cannot be empty!"
+            else:
+                if len(string) == 0:
+                    string = ""
+                results = matchMany([infix], [string])
+                processResults(results, isPlotting, isWriteToFile)
 
         elif(selection == '2'):
-            print "You selected 2"
-            infixes = ["1.0.1*", "a.((b.c)|b)*", "a.(b.b)*.c"]
-            strings = ["", "101", "abbc", "abcc", "abad", "abbbc"]
+            print "You selected 2 - Specify a file containing infixes and a file containing strings"
+            infix_file_name = raw_input("Please enter Infix file name: ")
+            string_file_name = raw_input("Please enter String file name: ")
+            
+            if(len(infix_file_name) > 0 and len(string_file_name) > 0):
+                infixes = utils.fr.readLinesToList(infix_file_name)
+                strings = utils.fr.readLinesToList(string_file_name)
+
+                results = matchMany(infixes, strings)
+                processResults(results, isPlotting, isWriteToFile)
+            else:
+                print "Neither Infix filename nor String filename can be empty!"
+
+        elif(selection == '3'):
+            print "You selected 3"
+            infixes = ["a*", "a.b.c*", "a.((b.c)|b)*", "a.(b.b)*.c"]
+            strings = ["", "abc", "abbc", "abcc", "abad", "abbbc"]
             results = matchMany(infixes, strings)
-            displayResults(results)
-            plotResults(results)
+            processResults(results, isPlotting, isWriteToFile)
 
-        elif(selection == '8'):
-            print "You selected 8"
-            plotter.testPlot()
 
-        elif(selection == '9'):
-            print "You selected 9"
-            testFileReader()
+        # Options
+        ## toggle plotting chart
+        elif(selection == 'p'):
+            print "You selected 'p' - toggle Plotting"
+            isPlotting = not isPlotting
+            print "Write To File = " + str(isPlotting)
+        ## toggle writing to file
+        elif(selection == 'w'):
+            print "You selected 'w' - output to csv file"
+            isWriteToFile = not isWriteToFile
+            if(isWriteToFile):
+                output_file_name = raw_input('Please enter the output file name: ')
+                print "New output file: " + output_file_name
+            else:
+                output_file_name = None
+            print "Write To File = " + str(isWriteToFile)
 
+        # quit application
         elif(selection == 'q'):
             keepRunning = False
 
         else:
             print "Invalid Selection, please try again"
-
-def testFileReader():
-    infixes = utils.fr.readLinesToList("infixes.txt")
-    strings = utils.fr.readLinesToList("strings.txt")
-
-    results = matchMany(infixes, strings)
-    #displayResults(results)
-    plotResults(results)
-
-def runMatchOneTest():
-    infix = "(a.(b|d))*"
-    stringToMatch = "abc"
-    print re_matcher.match(infix, stringToMatch), infix, stringToMatch
-
     
 def matchMany(infix_list, string_list):
     """Generate Results for a list of strings against a list of infixes"""
@@ -68,17 +104,28 @@ def matchMany(infix_list, string_list):
             result_list.append(r)
     return result_list
 
-def saveResultsToCsv(results_list):
+def processResults(result_list, isPlotting, isWriteToFile):
+    """Decide what to do with the results based on the menu options toggled"""
+    displayResults(result_list)
+    if(isPlotting):
+        plotResults(result_list)
+    if(isWriteToFile):
+        saveResultsToCsv(result_list)
+
+def saveResultsToCsv(result_list):
+    """Output the results to a CSV file"""
     utils.fr.writeToCsv(result_list)
 
 def plotResults(result_list):
+    """Creates a chart for each result"""
     for r in result_list:
         plot(r.thompsonsConstruction)
 
 def displayResults(result_list):
-    """Displays a list of results to the console"""
+    """Displays each result to the console"""
     for r in result_list:
         r.printResult()
 
+# call main
 if __name__ == "__main__":
     main()
