b9374f2560bfcd17b4969566874063500355f88f
SerjiVutinss <SerjiVutinss@gmail.com>
2019-03-31 22:39:25 +0100

    Fixed file read by stripping lines as they are read - tested  using infixes.txt and strings.txt outputting results to results.csv

--- GraphTheory_Project/utils/file_utils.py
+++ GraphTheory_Project/utils/file_utils.py
@@ -5,7 +5,7 @@ def readLinesToList(file_name):
     file = open(file_name, "r")
     line_list = list()
     for s in file.readlines():
-        line_list.append(s)
+        line_list.append(s.strip())
     return line_list
 
 def writeToCsv(result_list, file_name):
